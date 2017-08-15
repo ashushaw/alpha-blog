8b2bbb9deaafededadd24b66b4ba786089043331
ashushaw <ashu.shaw09@gmail.com>
2017-08-15 19:39:50 +0530

    Adding Login Feature

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ get 'about', to: 'pages#about'
 get 'signup', to: 'users#new'
 
 resources :users, except: [:new]
+
+get 'login', to: 'sessions#new'
+
+post 'login', to: 'sessions#create'
+
+delete 'logout', to: 'sessions#destroy'
 end
